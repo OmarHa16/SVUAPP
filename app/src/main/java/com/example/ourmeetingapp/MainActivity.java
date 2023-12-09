3528dfb5f05aa4ec819a972baef2f66f6866c773
OmarHa16 <ohassoun981@gmail.com>
2023-12-09 10:32:43 -0500

    here

--- app/src/main/java/com/example/ourmeetingapp/MainActivity.java
+++ app/src/main/java/com/example/ourmeetingapp/MainActivity.java
@@ -16,6 +16,7 @@ import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private DrawerLayout drawerLayout;
+    private String x = "soso";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
