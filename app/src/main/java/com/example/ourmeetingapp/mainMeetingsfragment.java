ebb603fc2cb5afb2aa9b4e15482b2f54ade37845
OmarHa16 <ohassoun981@gmail.com>
2023-12-11 13:15:08 -0500

    bottom nav bar

--- app/src/main/java/com/example/ourmeetingapp/mainMeetingsfragment.java
+++ app/src/main/java/com/example/ourmeetingapp/mainMeetingsfragment.java
@@ -5,13 +5,44 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.ourmeetingapp.databinding.ActivityMainBinding;
+import com.example.ourmeetingapp.databinding.FragmentMainMeetingsfragmentBinding;
+
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
 public class mainMeetingsfragment extends Fragment {
+    FragmentMainMeetingsfragmentBinding binding;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main_meetingsfragment, container, false);
+        binding = FragmentMainMeetingsfragmentBinding.inflate(inflater, container, false);
+
+        R_F(new mainmeetings());
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+
+            if(item.getItemId() == R.id.todaysmeetings){
+                R_F(new mainmeetings());
+            } else if (item.getItemId() == R.id.forwordmeetings) {
+                R_F(new forwordmeetings());
+            } else if (item.getItemId() == R.id.outlinemeetings) {
+                R_F(new outlinemeetings());
+            }
+
+
+            return true;
+        });
+        // Access views in your layout
+        return binding.getRoot();
+    }
+
+    private void R_F(Fragment fragment){
+        FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.framelayout,fragment);
+        fragmentTransaction.commit();
     }
 }
